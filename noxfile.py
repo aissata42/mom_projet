3717fa31d865617a26340d582bfb6d8d883c7765
aissata ly <aissatademly@gmail.com>
2025-06-21 13:37:14 +0000

    Ajout des fichiers de devoir

---
+++ noxfile.py
@@ -0,0 +1,7 @@
+import nox
+
+@nox.session
+def tests(session):
+    """Run the test suite."""
+    session.install(".[test]")  # installe les dépendances du groupe [test]
+    session.run("pytest")
